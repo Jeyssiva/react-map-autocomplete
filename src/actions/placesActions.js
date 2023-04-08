f51f95835cc6be1e9120cd0b3c99e3c4e2edf8d1
Jeyalakshmi R <jeyalakshmi.r@wins.local>
2023-04-08 14:54:38 +0530

    Google places

--- src/actions/placesActions.js
+++ src/actions/placesActions.js
@@ -8,10 +8,8 @@ export const fetchPlaces = (query = 'Batu') => async (dispatch) => {
     //   .then((data) => {
     //     dispatch({ type: FETCH_PLACES_SUCCESS, payload: data.results });
     //   });
-    const googleURL = '/api/place/textsearch';
-    
-    // process.env.NODE_ENV === 'development' ? '/api/place/textsearch' :
-    // `${process.env.REACT_APP_GOOGLE_MAP_URL}/api/place/textsearch`
+    const googleURL = process.env.NODE_ENV === 'development' ? '/api/place/textsearch' :
+    `${process.env.REACT_APP_GOOGLE_MAP_URL}/api/place/textsearch`
     console.log(googleURL)
     console.log('sending1234d44s')
      await fetch(`${googleURL}/json?query=${query}&key=${process.env.REACT_APP_GOOGLE_API_KEY}`,

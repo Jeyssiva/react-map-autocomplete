dd19cd57fc991f11477ba25dc79dd9f007d53763
Jeyalakshmi R <jeyalakshmi.r@wins.local>
2023-04-08 13:18:46 +0530

    Updates

--- precache-manifest.81e13f05ae9475b3f3f03048f0694942.js
+++ precache-manifest.81e13f05ae9475b3f3f03048f0694942.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2bec42976500a4033020bc9d37fe4d6a",
+    "revision": "8a5b5273c99a81d171c68ff35b8cbc7e",
     "url": "/react-map-autocomplete/index.html"
   },
   {
-    "revision": "e0f9684ce97bc3d5af42",
+    "revision": "bd8fe8f72e6d33a47416",
     "url": "/react-map-autocomplete/static/css/main.a7cc73ec.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-map-autocomplete/static/js/2.425b1dcf.chunk.js"
   },
   {
-    "revision": "e0f9684ce97bc3d5af42",
-    "url": "/react-map-autocomplete/static/js/main.7028145d.chunk.js"
+    "revision": "bd8fe8f72e6d33a47416",
+    "url": "/react-map-autocomplete/static/js/main.8732d173.chunk.js"
   },
   {
     "revision": "e54233991948db7bea26",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-map-autocomplete/precache-manifest.0f9b3d580852cf11bac9f43a466fd98b.js"
+  "/react-map-autocomplete/precache-manifest.81e13f05ae9475b3f3f03048f0694942.js"
 );
 
 self.addEventListener('message', (event) => {
